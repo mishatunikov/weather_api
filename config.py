2eacbbe7e5cefb9d00c85d360890df6511bf4346
mishatunikov <michael.tunikov@gmail.com>
2025-06-07 15:53:16 +0300

    feat: create config module for load env environs

---
+++ config.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+
+from environs import Env
+
+
+@dataclass
+class DjangoSetting:
+    """Django configuration data"""
+
+    secret_key: str
+    debug: bool
+
+
+@dataclass
+class OpenWeather:
+    """OpenWeather configuration data."""
+
+    api_key: str
+    url: str
+
+
+@dataclass
+class Config:
+    """Project configration data."""
+
+    django_settings: DjangoSetting
+    open_weather: OpenWeather
+
+
+def load_config() -> Config:
+    """Load environment variables from .env file."""
+
+    env = Env()
+    env.read_env()
+    return Config(
+        DjangoSetting(
+            secret_key=env.str('SECRET_KEY', 'SECRET_KEY'),
+            debug=env.bool('DEBUG'),
+        ),
+        OpenWeather(
+            api_key=env.str('open_weather_key'),
+            url=env.str('open_weather_url')
+        ),
+    )
+
+
+config: Config = load_config()
