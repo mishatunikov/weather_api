bdd8c8fff9f016f573dc7e423d69552360a3a3c0
mishatunikov <michael.tunikov@gmail.com>
2025-06-08 15:08:25 +0300

    feat: register weather/forcast/ endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
-from api.views import CurrentWeatherView
+from api.views import CurrentWeatherView, ForecastWeatherView
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('weather/current/', CurrentWeatherView.as_view()),
+    path('weather/forcast/', ForecastWeatherView.as_view()),
 ]
