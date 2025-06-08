73574ce35be6f80576a83c62488f528e83701993
mishatunikov <michael.tunikov@gmail.com>
2025-06-08 15:07:28 +0300

    feat: create ForecastGetSerializer

---
+++ api/serializers.py
@@ -0,0 +1,35 @@
+from datetime import timedelta
+
+from api import consts
+from django.utils import timezone
+from rest_framework import serializers
+from rest_framework.validators import ValidationError
+
+
+class ForecastGetSerializer(serializers.Serializer):
+    city = serializers.CharField(required=True)
+    date = serializers.DateField(
+        required=True,
+        format=consts.DATE_FORMAT,
+        input_formats=[consts.DATE_FORMAT],
+    )
+
+    def validate_date(self, value):
+        if value < timezone.now().date():
+            raise ValidationError(
+                detail={'message': 'Дата не может быть прошлом.'}
+            )
+
+        if value - timezone.now().date() > timedelta(10):
+            raise ValidationError(
+                detail={
+                    'message': 'Дата не может быть в будущем больше, '
+                    'чем на 10 дней.'
+                }
+            )
+
+        return value
+
+
+class ForecastCreateSerializer(serializers.Serializer):
+    pass
